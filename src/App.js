6fe3f52fe1cf4a35992cc43dab313f5c6812cab3
Alex Kayser <akctba@gmail.com>
2022-11-20 00:36:00 -0500

    audio component

--- src/App.js
+++ src/App.js
@@ -7,15 +7,24 @@ function App() {
       <header className="App-header">
         <img src={logo} className="wobble" alt="logo" />
 
+          <Player track="rain" />
 
+          <Player track="utero" />
 
       </header>
     </div>
   );
 }
 
-function player(...props) {
-  
+function Player(props) {
+  return (
+    <div className="player-box">
+      <h2 className="track-title">{props.track}</h2>
+      <audio controls loop src={"/audio/"+props.track+".mp3"} >
+        Your browser does not support the audio tag.
+      </audio>
+    </div>
+  )
 }
 
 export default App;

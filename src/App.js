65f2e7d04e0082cbc1e8b3ff3acbcdde09d0eb75
Alex Kayser <akctba@gmail.com>
2022-11-20 11:10:28 -0500

    adding tracks

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="wobble" alt="logo" />
 
+          <Player track="Sleep-little-one" />
+          <Player track="Baby-Sleep-Songs" />
+          <Player track="Collection-for-Nursery" />
           <Player track="rain" />
-
           <Player track="utero" />
 
       </header>
@@ -19,12 +21,16 @@ function App() {
 function Player(props) {
   return (
     <div className="player-box">
-      <h2 className="track-title">{props.track}</h2>
+      <h2 className="track-title">{nameFix(props.track)}</h2>
       <audio controls loop src={"/audio/"+props.track+".mp3"} >
         Your browser does not support the audio tag.
       </audio>
     </div>
   )
+}
+
+function nameFix(name) {
+  return name.replaceAll('-', ' ')
 }
 
 export default App;
